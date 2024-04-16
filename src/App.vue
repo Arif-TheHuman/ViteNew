d0768ba5af0601099ff016ebef3c4c088ac3badd
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-04-17 00:14:42 +0800

    Import Tailwind CSS and modified profile pic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div>
-      <img src="./assets/images/profile.jpeg" class="logo vue" alt="Profile Picture" />
+  <div class="container">
+    <div class="profile">
+      <img class="logo"src="./assets/images/profile.jpeg" alt="Profile Picture" />
+      <div class="menacing" id="menacing1">
+        <img src="./assets/images/menacing.png" alt="Menacing..." class="menacess">
+      </div>
+      <div class="menacing" id="menacing2">
+        <img src="./assets/images/menacing.png" alt="Menacing..." class="menacess">
+      </div>
+      <div class="menacing" id="menacing3">
+        <img src="./assets/images/menacing.png" alt="Menacing..." class="menacess">
+      </div>
+      <div class="menacing" id="menacing4">
+        <img src="./assets/images/menacing.png" alt="Menacing..." class="menacess">
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+
+.menacess {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 600ms;
+}
+
+.container:hover .menacess {
+  opacity: 1;
+  animation: shake 0.5s; /* Apply the shake animation */
+  animation-iteration-count: infinite;
+}
+
+#menacing1 .menacess {
+  top: -10px;
+  left: -10px;
+}
+
+#menacing2 .menacess {
+  top: -10px;
+  right: -10px;
+}
+
+#menacing3 .menacess {
+  bottom: -10px;
+  left: -10px;
+}
+
+#menacing4 .menacess {
+  bottom: -10px;
+  right: -10px;
+}
+
 .logo {
-  border-radius: 50%; /* Make the image circular */
-  height: 20vw; /* Set the height relative to the viewport width */
-  max-height: 200px; /* Set a maximum height */
-  min-height: 100px; /* Set a minimum height */
+  border-radius: 50%;
+  height: 20vw;
+  max-height: 200px;
+  min-height: 100px;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-  object-fit: cover; /* Ensure the image covers the entire area of the div */
+  object-fit: cover;
 }
+
+.profile {
+  position: relative;
+  border-radius: 50%;
+  height: 20vw;
+  max-height: 200px;
+  min-height: 100px;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+  object-fit: cover;
+}
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #5900b3aa);
 }
 </style>
