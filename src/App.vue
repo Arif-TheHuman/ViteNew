5eceff44e1ddff30cbe4cd0c4b9585e27e3c3e08
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-04-16 09:37:11 +0800

    Update profile picture styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,14 @@
 
 <style scoped>
 .logo {
-  height: 6em;
+  border-radius: 50%; /* Make the image circular */
+  height: 20vw; /* Set the height relative to the viewport width */
+  max-height: 200px; /* Set a maximum height */
+  min-height: 100px; /* Set a minimum height */
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  object-fit: cover; /* Ensure the image covers the entire area of the div */
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
