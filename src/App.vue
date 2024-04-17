e3d3ce0ade1ec1e07a97264d9fdbbf35ad033aca
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-04-17 11:15:35 +0800

    Refactor profile picture styling and add hover effect in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="flex">
-  <br>
-  <div class="relative flex justify-start md:justify-center lg:justify-start items-center">
-    <div class="relative w-64 h-64 mx-auto md:w-72 md:h-72 lg:w-80 lg:h-80">
-      <img class="w-full h-full object-cover rounded-full hover:shadow-purple-glow transition-shadow duration-500 ease-in-out" src="./assets/images/profile.jpg" alt="Profile Picture" 
-           @mouseover="isHovered = true" 
-           @mouseleave="isHovered = false" />
-      <div class="absolute top-0 left-0 w-12 h-12 transition-opacity duration-100 linear" 
-           :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing1">
-        <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
-      </div>
-      <div class="absolute top-0 right-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
-           :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing2">
-        <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
-      </div>
-      <div class="absolute bottom-0 left-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
-           :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing3">
-        <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
-      </div>
-      <div class="absolute bottom-0 right-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
-           :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing4">
-        <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
+  <div class="flex flex-col md:flex-row">
+    <br>
+    <div class="relative flex justify-start md:justify-center lg:justify-start items-center mb-4 md:mb-0">
+      <div class="relative w-64 h-64 mx-auto md:w-72 md:h-72 lg:w-80 lg:h-80">
+        <img class="w-full h-full object-cover rounded-full hover:shadow-purple-glow transition-shadow duration-500 ease-in-out" src="./assets/images/profile.jpg" alt="Profile Picture" 
+             @mouseover="isHovered = true" 
+             @mouseleave="isHovered = false" />
+        <div class="absolute top-0 left-0 w-12 h-12 transition-opacity duration-100 linear" 
+             :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing1">
+          <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
+        </div>
+        <div class="absolute top-0 right-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
+             :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing2">
+          <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
+        </div>
+        <div class="absolute bottom-0 left-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
+             :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing3">
+          <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
+        </div>
+        <div class="absolute bottom-0 right-0 w-12 h-12 transition-opacity duration-600 ease-in-out" 
+             :class="{'opacity-0': !isHovered, 'opacity-100': isHovered, 'animate-shake': isHovered}" id="menacing4">
+          <img src="./assets/images/menacing.png" alt="Menacing..." class="w-full h-full object-cover transition duration-200 ease-in-out">
+        </div>
       </div>
     </div>
-  </div>
-  <span class="pl-20">
-    <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold text-purple-600">Arif-TheHuman</h1>
-    <p class="pt-3">Hello! My real name is Muhammad Nur Arif Bin Abdullah Hidup.</p>
-    <p class="pt-3">Certified Cat Lover</p>
-    <p class="pt-3">Professional Human, 10+ Years of Experience</p>
-    <p class="pt-3">Hover On My Profile To See Something Cool!</p>
-  </span>
+    <span class="pl-10 md:pl-20">
+      <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold text-purple-600">Arif-TheHuman</h1>
+      <p class="pt-3">Hello! My real name is Muhammad Nur Arif Bin Abdullah Hidup.</p>
+      <p class="pt-3">Certified Cat Lover</p>
+      <p class="pt-3">Professional Human, 10+ Years of Experience</p>
+      <p class="pt-3">Hover On My Profile To See Something Cool!</p>
+    </span>
 </div>
 </template>
 
